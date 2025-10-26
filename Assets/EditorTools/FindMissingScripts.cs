84e34d93be53cf9ba2d6d9b2049fdd43a4bc0b63
Joren Van Borm <jorenvanborm@skynet.be>
2025-10-26 13:27:16 +0100

    Fix: no longer double-check selected items if an ancestor is also selected

--- Assets/EditorTools/FindMissingScripts.cs
+++ Assets/EditorTools/FindMissingScripts.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEditor;
+using System.Collections.Generic;
+using System.Linq;
 
 // Based on:
 // https://web.archive.org/web/20130514114424/http://wiki.unity3d.com/index.php/FindMissingScripts
@@ -7,7 +9,12 @@ using UnityEditor;
 
 public static class FindMissingScriptsRecursively
 {
-    static int go_count = 0, components_count = 0, missing_count = 0;
+    // pass-by-reference data class to track search stats
+    private class SearchResults
+    {
+        public int go_count = 0, components_count = 0, missing_count = 0;
+        public HashSet<GameObject> searched = new();
+    }
 
     // validator function (menu item will be greyed out if this returns False)
     [MenuItem("Tools/Argo's Grab Bag/Find Missing Scripts", validate = true)] // standard grab bag location
@@ -19,10 +26,10 @@ public static class FindMissingScriptsRecursively
 
     [MenuItem("Tools/Argo's Grab Bag/Find Missing Scripts")] // standard grab bag location
     [MenuItem("GameObject/Find Missing Scripts")] // adds to menu bar and context menu
-    static void FindMissingScripts(MenuCommand command)
+    static void MenuItem(MenuCommand command)
     {
-        // When invoked from menu bar, FindInSelected() runs once without context.
-        // When invoked from context menu, it runs once for each selected item (setting it as its context)
+        // When invoked from menu bar, MenuItem() runs once without context.
+        // When invoked from context menu, it runs once for each selected item (setting that item as its context)
 
         // only run the search once, across all selected objects
         // (do NOT compare to Selection.activeGameObject as it may be null)
@@ -35,34 +42,47 @@ public static class FindMissingScriptsRecursively
     private static void FindInSelected()
     {
         GameObject[] go = Selection.gameObjects;
-        go_count = 0;
-        components_count = 0;
-        missing_count = 0;
+        SearchResults results = new();
+        Debug.LogFormat("Start search of [{0}]", string.Join(", ", go.Select(x => $"\"{x.name}\"")));
         foreach (GameObject g in go)
         {
-            FindInGO(g);
+            FindInGO(g, results);
         }
-        Debug.Log(string.Format("Searched {0} GameObjects, {1} components, found {2} missing scripts", go_count, components_count, missing_count));
+        Debug.LogFormat
+        (
+            "Searched {0} GameObjects, {1} components, found {2} missing scripts (see warnings above)",
+            results.go_count,
+            results.components_count,
+            results.missing_count
+        );
     }
 
-    private static void FindInGO(GameObject g)
+    private static void FindInGO(GameObject g, SearchResults results)
     {
-        go_count++;
+        if (results.searched.Contains(g))
+        {
+            // if this GO has been searched already, so have all it's children
+            return;
+        }
+
+        results.searched.Add(g);
+
+        results.go_count++;
         Component[] components = g.GetComponents<Component>();
         for (int i = 0; i < components.Length; i++)
         {
-            components_count++;
+            results.components_count++;
             if (components[i] == null)
             {
-                missing_count++;
-                Debug.Log(g.name + " has an empty script attached in position: " + i, g);
+                results.missing_count++;
+                Debug.LogWarning(g.name + " has an empty script attached in position: " + i, g);
             }
         }
         // Now recurse through each child GO (if there are any):
         foreach (Transform childT in g.transform)
         {
             //Debug.Log("Searching " + childT.name  + " " );
-            FindInGO(childT.gameObject);
+            FindInGO(childT.gameObject, results);
         }
     }
 }
